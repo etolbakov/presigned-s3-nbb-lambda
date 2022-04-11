b0d381de9e491ce484c1145c81b1f37167b4176c
Eugene Tolbakov <ev.tolbakov@gmail.com>
2022-04-11 17:31:27 +0100

    initial commit

---
+++ lambda-api-gw/handler.cljs
@@ -0,0 +1,32 @@
+(ns handler
+  (:require ["@aws-sdk/client-s3" :refer [S3Client]]
+            ["@aws-sdk/s3-presigned-post" :refer [createPresignedPost]]
+            [clojure.string :as s]
+            [goog.string.format]
+            [applied-science.js-interop :as j]
+            [promesa.core :as p]))
+
+(def s3 (S3Client. #js{:region "eu-west-1"}))
+
+(def bucket-name-template "%s-docs-upload-bucket")
+
+(defn handler [event _ctx]
+      (p/let [{:keys [env folderName fileName]} (-> event
+                                                    (j/get :pathParameters)
+                                                    (j/lookup))
+              key (str (s/replace folderName #"_" "/") "/" fileName)
+              bucket-name (goog.string/format bucket-name-template env)
+              response (createPresignedPost s3 (clj->js {:Bucket     bucket-name
+                                                         :Key        key
+                                                         :Expires    300
+                                                         :Fields     {:key key}
+                                                         :Conditions [["eq", "$key", key]
+                                                                      ["content-length-range", 0, 10485760]
+                                                                      ["starts-with", "$Content-Type", "text/"]]}))]
+             (clj->js {:statusCode 200
+                       :headers    {"Access-Control-Allow-Origin"  "*"
+                                    "Access-Control-Allow-Headers" "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent,Access-Control-Allow-Origin",
+                                    "Access-Control-Allow-Methods" "OPTIONS,GET"}
+                       :body       (js/JSON.stringify response nil 2)})))
+
+#js {:handler handler}
